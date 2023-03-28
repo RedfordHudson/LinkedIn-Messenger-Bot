20e8b731c2793194162806782516dad8e5718d99
RedfordHudson <redfordfoxhudson@gmail.com>
2023-03-28 00:07:58 -0700

    more robust

--- Driver.py
+++ Driver.py
@@ -19,6 +19,7 @@ class Driver():
         self.driver = webdriver.Chrome(options=options)
 
     def fe(self,path):
+        # *** GUARD AGAINST ELEMENT DNE
         return self.driver.find_elements(By.XPATH,path)
 
     def goToPage(self,tag,index):
@@ -26,12 +27,33 @@ class Driver():
 
     def getProfileLinks(self):
         
-        container = self.fe('//*[@id="main"]/div/div')[0]
+        # container = self.fe('//*[@id="main"]/div/div')[0]
 
-        profiles = container.find_elements(By.TAG_NAME,'li')
+        # profiles = container.find_elements(By.TAG_NAME,'li')
 
-        link = [profile.find_element(By.CLASS_NAME,'app-aware-link').get_attribute('href') for profile in profiles]
-        return link
+        # get list elements / profile containers
+        containers = self.driver.find_elements(By.XPATH,"//li[contains(@class,'reusable-search__result-container')]")
+
+        # extract anchors from profile containers
+        # anchors = [container.find_element(By.XPATH,".//div[contains(@class,'mb1')]//a") for container in containers]
+        anchors = []
+        for i,container in enumerate(containers):
+            anchor_elements = container.find_elements(By.XPATH,".//div[contains(@class,'mb1')]//a")
+            # if web element does not follow specified format, skip
+            if len(anchor_elements):
+                anchors.append(anchor_elements[0])
+
+        # remove "LinkedIn Member" profiles
+        anchors = list(filter(lambda container:container.get_attribute('innerText') != 'LinkedIn Member',anchors))
+        
+        # extract URL
+        links = [container.get_attribute('href') for container in anchors]
+        
+        # links = self.driver.find_elements(By.XPATH,"//div[contains(@class, 'search-results-container')]//a[contains(@class, 'app-aware-link')]")
+        # links = [links[i] for i in range(0,len(links),3)]
+        # links = [link.get_attribute('href') for link in links]
+
+        return links
 
     def getAllLinks(self,tag,num_pages):
         links = []
@@ -76,17 +98,23 @@ class Driver():
     def message(self,profile_link,subject_msg,body_msg):
         self.driver.get(profile_link)
 
+        self.closeConversation()
+
         last_name,position,firm = self.getPersonalData()
 
         # open message tab
-        href_container = self.driver.find_elements(By.CLASS_NAME,'pvs-profile-actions')[1]
-        message_href = href_container.find_elements(By.CLASS_NAME,'entry-point')
-        
-        # profile does not have 'message' button
-        if not len(message_href):
-            return
+        # href_container = self.driver.find_elements(By.CLASS_NAME,'pvs-profile-actions')
+        # # href_container = self.driver.find_elements(By.CLASS_NAME,'pvs-profile-actions')[1]
+        # message_href = href_container.find_elements(By.CLASS_NAME,'entry-point')
 
-        message_href[0].find_element(By.TAG_NAME,'a').click()
+        # profile does not have 'message' button
+        # if not len(message_href):
+        #     return
+
+        # message_href[0].find_element(By.TAG_NAME,'a').click()
+
+        buttons = self.driver.find_elements(By.XPATH,"//*[contains(@class, 'pvs-profile-actions')]//button")
+        buttons[1].click()
 
         # LINKEDIN MUST BE > SPLIT SCREEN
         iter = 0
@@ -109,10 +137,13 @@ class Driver():
 
         # hydrate input fields
         subject.send_keys(subject_msg)
-        body.send_keys(body_msg % (last_name,position,firm))
+        body.send_keys(body_msg % (last_name))
+        # body.send_keys(body_msg % (last_name,position,firm))
 
         # submit
         send_btn = message_container[2].find_element(By.CSS_SELECTOR,'button.msg-form__send-button')
+        
+        # time.sleep(10)
         send_btn.submit()      
 
 if __name__=='__main__':
--- Messenger.py
+++ Messenger.py
@@ -5,23 +5,31 @@ class Messenger:
     def __init__(self,csv_file):
         self.csv_file = csv_file
 
-        self.subject_msg = 'Restaurant Industry Market Research'
+        self.subject_msg = 'Dietetics Career Advice'
         self.body_msg = '''Hello Mx. %s,
 
-I am a student seeking to learn more about the restaurant industry. Specifically, I want to learn more about expenses incurred from servers dropping plates.
+My sister is an undergraduate studying dietetics and is looking for an internship this summer. She is new to the internship scene and doesn't have the resources to find an opportunity. I want to help her with that.
 
-May I have 10 minutes of your time to interview you about your experiences as %s at %s?'''
+May I have 10 minutes of your time to chat about your professional journey as a dietician?'''
 
         self.driver = Driver()
 
     def loadCSV(self,tag,num_pages):
         links = self.driver.getAllLinks(tag,num_pages)
+
+        # for debugging
+        # for i,l in enumerate(links):
+        #     print(i,l)
+
         pd.DataFrame({'name': links}).to_csv(self.csv_file, index=False)
 
     def message(self):
         links = pd.read_csv(self.csv_file)
 
-        for row in links[:5].iterrows():
+        for row in links.iterrows():
+            index = row[0]
+            print(index)
+
             link = row[1]['name']
             self.driver.message(link,self.subject_msg,self.body_msg)
 
@@ -31,7 +39,7 @@ May I have 10 minutes of your time to interview you about your experiences as %s
 if __name__=='__main__':
     messenger = Messenger('links.csv')
 
-    # messenger.loadCSV('restaurant',4)
+    # messenger.loadCSV('dietetics intern',5)
 
     messenger.message()
 
